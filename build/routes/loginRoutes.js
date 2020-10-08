abce69794bb500ff88155772afb7726460f29c78
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-10-08 15:18:06 -0700

    Add protected route and requireAuth middleware

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -2,6 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    else {
+        res.status(403);
+        res.send('Not permitted');
+    }
+}
 var router = express_1.Router();
 exports.router = router;
 router.get('/login', function (req, res) {
@@ -18,4 +28,19 @@ router.post('/login', function (req, res) {
     else {
         res.send('Invalid email or password');
     }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n        <div>\n            <div>You are logged in</div>\n            <a href=\"/logout\">Logout</a>\n        </div>\n    ");
+    }
+    else {
+        res.send("\n        <div>\n            <div>You are not logged in</div>\n            <a href=\"/login\">Login</a>\n        </div>\n    ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = null;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Wecome to protected route, logged in user');
 });
